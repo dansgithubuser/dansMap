777ed14c8b1c298d8cf1965372b3d30e20b31cd2
Dan <dansonlinepresence@gmail.com>
2018-05-18 14:32:13 -0400

    add notes to database logic

--- map/models.py
+++ map/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+	text=models.TextField()
+	created=models.DateTimeField()
+	user=models.ForeignKey(User, models.CASCADE)
