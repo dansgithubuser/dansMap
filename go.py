8541e59303ecacd7c52a37518974909a1c27754e
Dan <dansonlinepresence@gmail.com>
2018-05-18 13:34:34 -0400

    create user

--- go.py
+++ go.py
@@ -1,20 +1,33 @@
+from collections import defaultdict
 import argparse
+import os
 import subprocess
+import sys
 import time
 
 parser=argparse.ArgumentParser()
-parser.add_argument('--create-database', '-c', action='store_true')
+parser.add_argument('--create-database', action='store_true')
 parser.add_argument('--drop-database', action='store_true')
+parser.add_argument('--create-user', action='store_true')
 args=parser.parse_args()
 
 def invoke(*args): subprocess.check_call(args)
-def psql(command): invoke('sudo', '-i', '-u', 'postgres', 'psql', '-c', command)
+def psql(*args): invoke('sudo', '-i', '-u', 'postgres', 'psql', *args)
+def psqlc(command): psql('-c', command)
 
-if args.create_database: psql('CREATE DATABASE map_database')
+if args.create_database: psqlc('CREATE DATABASE map_database')
 
 if args.drop_database:
 	print('dropping in')
 	for i in range(5):
 		print(5-i)
 		time.sleep(1)
-	psql('DROP DATABASE map_database')
+	psqlc('DROP DATABASE map_database')
+
+if args.create_user:
+	sys.path.append(os.path.join('dansmap'))
+	import settings_secret
+	recursive_defaultdict_builder=lambda: defaultdict(recursive_defaultdict_builder)
+	configuration=recursive_defaultdict_builder()
+	settings_secret.inject(configuration)
+	psqlc("CREATE USER map_database_user WITH PASSWORD '{}';".format(configuration['DATABASES']['default']['PASSWORD']))
