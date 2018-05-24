7d6faca66cb4501654cf6da52a82d59970669b9f
Dan <dansonlinepresence@gmail.com>
2018-05-24 15:43:42 -0400

    add default to handle existing data and migrate
    django enforces a default value.
    It can be done outside code, but I don't like the whimsical nature of that idea.
    I had no data when attempting to migrate locally.

--- map/models.py
+++ map/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Note(models.Model):
 	text=models.TextField()
-	latitude=models.DecimalField(max_digits=9, decimal_places=6)
-	longitude=models.DecimalField(max_digits=9, decimal_places=6)
+	latitude=models.DecimalField(max_digits=9, decimal_places=6, default=0)
+	longitude=models.DecimalField(max_digits=9, decimal_places=6, default=0)
 	created=models.DateTimeField(auto_now_add=True)
 	user=models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
 
