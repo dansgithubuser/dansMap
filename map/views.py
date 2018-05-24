2884530b278628dbe522c50d5d33e7d9bbe8c285
Dan <dansonlinepresence@gmail.com>
2018-05-23 23:58:52 -0400

    template

--- map/views.py
+++ map/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 import json
 from .models import Note
 
 def index(request):
-	return HttpResponse('INDEXX')
+	return HttpResponse(loader.get_template('map/index.html').render({}, request))
 
 def note_new(request):
 	note=Note(text=request.POST['text'])
